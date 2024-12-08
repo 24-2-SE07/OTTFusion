62f5e83a945ba6fbaa23f370b5cd21aefadd9b2c
tlrznf46 <tlrznf46@gmail.com>
2024-12-08 22:34:40 +0900

    Add : Heart Icon

--- MainPage/main.js
+++ MainPage/main.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
       const contentHTML = `
         <div class="content-item">
           <img src="../image/image.png" alt="${content.title}" class="content-image">
+          <i class="far fa-heart"></i>
           <h2 class="content-title">${content.title}</h2>
           <div class="rating">${content.rating}</div>
           <p class="platform">플랫폼: ${content.platform}</p>
@@ -14,4 +15,12 @@ $(document).ready(function () {
       `;
       $contentGrid.append(contentHTML);
     });
+    
+    $(".content-item").on("click", ".fa-heart", function () {
+        if ($(this).hasClass("far")) {
+          $(this).removeClass("far").addClass("fas"); // 빈 하트 -> 꽉 찬 하트
+        } else {
+          $(this).removeClass("fas").addClass("far"); // 꽉 찬 하트 -> 빈 하트
+        }
+      });
 });
